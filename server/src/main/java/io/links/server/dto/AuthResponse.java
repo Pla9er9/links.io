3c91ec0ac7c13fb02d87ba68ce5d42be83f751ae
Pla9er <drugiekonto_99@o2.pl>
2024-05-16 22:05:45 +0200

    create: authentication response dto

---
+++ server/src/main/java/io/links/server/dto/AuthResponse.java
@@ -0,0 +1,12 @@
+package io.links.server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String id;
+    private String username;
+    private String token;
+}
