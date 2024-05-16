7fd18334842098ddc551a307326880b8666fcec0
Pla9er <drugiekonto_99@o2.pl>
2024-05-16 20:38:20 +0200

    create: user model

---
+++ server/src/main/java/io/links/server/model/User.java
@@ -0,0 +1,49 @@
+package io.links.server.model;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+@Document("user")
+@Data
+@Builder
+public class User implements UserDetails {
+    @Id
+    private String id;
+    private String username;
+    private String email;
+    private String password;
+    private boolean isEmailVerified;
+    private LocalDateTime joinedDateTime;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
