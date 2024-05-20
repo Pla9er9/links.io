0a112814f2f7a882ab3055e6b8b7958a34226fbc
Pla9er <drugiekonto_99@o2.pl>
2024-05-20 18:35:29 +0200

    add: redis config and enabled caching

--- server/src/main/java/io/links/server/ServerApplication.java
+++ server/src/main/java/io/links/server/ServerApplication.java
@@ -2,9 +2,11 @@ package io.links.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableCaching
 @EnableMongoRepositories(basePackages = "io.links.server.repository")
 public class ServerApplication {
 
