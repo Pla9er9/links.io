4e021455b73b122985ee22106ba8e7b7a0a0753a
Pla9er <drugiekonto_99@o2.pl>
2024-05-21 21:07:26 +0200

    add: notnull annotations

--- server/src/main/java/io/links/server/dto/UserLoginRequest.java
+++ server/src/main/java/io/links/server/dto/UserLoginRequest.java
@@ -1,5 +1,6 @@
 package io.links.server.dto;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,8 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserLoginRequest {
+    @NotNull
     @Size(min = 3, max = 22, message = "Username must be between 6 and 50 characters")
     private String username;
+    @NotNull
     @Size(min = 6, max = 50, message = "Password must be between 6 and 50 characters")
     private String password;
 }
--- server/src/main/java/io/links/server/dto/UserRegistrationRequest.java
+++ server/src/main/java/io/links/server/dto/UserRegistrationRequest.java
@@ -1,6 +1,7 @@
 package io.links.server.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,10 +11,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRegistrationRequest {
+    @NotNull
     @Size(min = 3, max = 22, message = "Username must be between 6 and 50 characters")
     private String username;
+    @NotNull
     @Email(message = "Email must be valid")
     private String email;
+    @NotNull
     @Size(min = 6, max = 50, message = "Password must be between 6 and 50 characters")
     private String password;
 }
