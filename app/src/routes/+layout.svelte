2600e69a4f990a7dac015b6826ad0663e29e42a8
Pla9er9 <drugiekonto_99@o2.pl>
2024-05-29 08:20:19 +0200

    create: layout with dialog and navbar

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Dialog from "$components/Dialog.svelte";
+	import Navbar from "$components/Navbar.svelte";
+</script>
+
+<div class="max-w-[1200px] mx-auto">
+	<Navbar />
+	<Dialog />
+	<slot></slot>
+</div>
