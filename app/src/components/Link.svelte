0a3c54442175233c857d508f78c678fc3c144056
Pla9er <drugiekonto_99@o2.pl>
2024-06-05 00:01:09 +0200

    update: user profile page ui

---
+++ app/src/components/Link.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	export let text: string;
+	export let url: string;
+
+	import { LinkOutline } from "flowbite-svelte-icons";
+
+</script>
+
+<a href={url} class="row" target="_blank">
+	<LinkOutline />
+	<span class="mx-auto">{text}</span>
+</a>
+
+<style lang="scss">
+	a {
+		width: 80vw;
+		max-width: 400px;
+		height: 55px;
+		outline: solid 1px pink;
+		background-color: #ffffff08;
+		margin: 18px 0;
+		border-radius: 25px;
+		justify-content: center;
+		transition: 250ms ease-in-out;
+		color: #fff;
+		padding: 0 20px;
+		box-sizing: border-box;
+
+		&:hover {
+			background-color: rgb(251, 220, 224);
+			outline: solid 2px #000;
+			color: #000;
+			font-weight: 600;
+		}
+	}
+</style>
--- app/src/routes/[username]/+page.svelte
+++ app/src/routes/[username]/+page.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
-    export let data
+	import Link from '$components/Link.svelte';
+	import { Avatar } from 'flowbite-svelte';
 
-    const profile = data.profile)dsadasdas
+	export let data;
+
+	const profile = data.profile;
 </script>
 
-<div class="column">
-    <h1>{data.profile.username}</h1>
-    <p>{description}</p>
-    {#each data.profile.links as link}
-        <a href="{link}">{link}</a>
-    {/each}
+<div class="column mt-16">
+	<Avatar size="xl">
+		<span class="text-5xl">{profile.username.slice(0, 2)}</span>
+	</Avatar>
+	<h1 class="mt-8 text-5xl font-black text-pink-100">{profile.username}</h1>
+	<p class="text-medium mb-12 mt-4 text-gray-400">{profile.description}</p>
+	{#each profile.links as link}
+		<Link url="https://{link.url}" text={link.text} />
+	{/each}
 </div>
