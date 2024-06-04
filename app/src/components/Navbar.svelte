eadf80736e7b00d4d658e273e0cd1b6835731d36
Pla9er <drugiekonto_99@o2.pl>
2024-06-04 13:34:50 +0200

    change: navbar for authenticated user

--- app/src/components/Navbar.svelte
+++ app/src/components/Navbar.svelte
@@ -1,8 +1,14 @@
-<script>
-	import { LinkOutline } from 'flowbite-svelte-icons';
+<script lang="ts">
+	import { CogOutline, LinkOutline } from 'flowbite-svelte-icons';
 	import '../app.css';
-	import { Navbar, NavBrand, Button } from 'flowbite-svelte';
+	import { Navbar, NavBrand, Button, Avatar } from 'flowbite-svelte';
 	import { updateDialog } from '$lib/dialogStore';
+	import { userStore, type userData } from '$lib/userStore';
+
+	let user: userData;
+	userStore.subscribe((u) => {
+		user = u;
+	});
 </script>
 
 <Navbar rounded class="mx-auto px-4 py-4" style="background: transparent">
@@ -10,5 +16,20 @@
 		<LinkOutline size="lg" color="pink" />
 		<i class="ml-2 text-2xl font-semibold text-white">Links.io</i>
 	</NavBrand>
-	<Button pill outline on:click={() => updateDialog('registration')}>Sign In</Button>
+	{#if user.username}
+		<div class="row space-x-4">
+			<a href="/settings">
+				<Button outline class="w-12 rounded-3xl">
+					<CogOutline color="#fff" size="md" />
+				</Button>
+			</a>
+			<a href="/{user.username}">
+				<Button outline class="rounded-3xl">
+					<span class="text-white">{user.username}</span>
+				</Button>
+			</a>
+		</div>
+	{:else}
+		<Button pill outline on:click={() => updateDialog('registration')}>Sign In</Button>
+	{/if}
 </Navbar>
