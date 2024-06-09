3aa9171b9a4b5ffa66ec0567e4546789423c7dc2
Pla9er <drugiekonto_99@o2.pl>
2024-06-09 23:35:13 +0200

    create: LinksEditor component for editing profile links in setting page

---
+++ app/src/components/LinksEdittor.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { Button, Card, Input } from 'flowbite-svelte';
+	import { LinkOutline } from 'flowbite-svelte-icons';
+
+	export let links: { text: string; url: string }[];
+
+	function addLink() {
+		links = [...links, { text: '', url: '' }];
+	}
+
+	function deleteAll() {
+		links = [];
+	}
+</script>
+
+{#each links as link}
+	<div class="row w-full space-x-2" style="margin: 15px 0;">
+		<Button color="alternative" checked style="cursor: default" class="w-4">
+			<LinkOutline />
+		</Button>
+		<Input placeholder="Name" bind:value={link.text} />
+		<Input placeholder="Url" bind:value={link.url} />
+	</div>
+{/each}
+<div class="row mb-4 space-x-2">
+	<Button outline on:click={addLink}>Add link</Button>
+	{#if links.length > 0}
+		<Button color="red" outline on:click={deleteAll}>Delete all</Button>
+	{/if}
+</div>
