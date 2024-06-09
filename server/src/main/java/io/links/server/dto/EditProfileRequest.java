3912a6dbb21dec0fe5716e78cb4e4c1cb46231d2
Pla9er <drugiekonto_99@o2.pl>
2024-06-09 23:30:43 +0200

    change: add username and set description length limit at EditProfileRequest

--- server/src/main/java/io/links/server/dto/EditProfileRequest.java
+++ server/src/main/java/io/links/server/dto/EditProfileRequest.java
@@ -1,5 +1,6 @@
 package io.links.server.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.links.server.model.Link;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -12,6 +13,10 @@ import java.util.List;
 @AllArgsConstructor
 public class EditProfileRequest {
     @NotNull
+    @Size(min = 3, max = 22, message = "Username must be between 6 and 50 characters")
+    private String username;
+    @NotNull
+    @Size(max = 150)
     private String description;
     @NotNull
     @Size(max = 6)
