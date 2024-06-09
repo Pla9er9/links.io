4ab4a320fbc837b041ab27ceee647c8b08cd00e0
Pla9er <drugiekonto_99@o2.pl>
2024-06-09 23:33:13 +0200

    add: succes variant to toast and delete unused default Options of toast

--- app/src/components/Toast.svelte
+++ app/src/components/Toast.svelte
@@ -1,20 +1,40 @@
 <script lang="ts">
 	import { toastStore, type toast } from '$lib/toastStore';
 	import { Toast } from 'flowbite-svelte';
-	import { CloseCircleSolid } from 'flowbite-svelte-icons';
+	import { CheckCircleSolid, CloseCircleSolid } from 'flowbite-svelte-icons';
 	import { slide } from 'svelte/transition';
 
 	let toasts: toast[];
 	toastStore.subscribe((t) => {
 		toasts = t;
 	});
+
+	function getColorOfVariant(variant: string | null): 'red' | 'green' | undefined {
+		if (!variant) {
+			return 'red';
+		}
+
+		switch (variant) {
+			case 'danger':
+				return 'red';
+			case 'succes':
+				return 'green';
+			default:
+				return undefined;
+		}
+	}
 </script>
 
 {#each toasts as t}
-	<Toast transition={slide} position="bottom-right" color="red">
+	<Toast transition={slide} position="bottom-right" color={getColorOfVariant(t.variant)}>
 		<svelte:fragment slot="icon">
-			<CloseCircleSolid class="h-5 w-5" />
-			<span class="sr-only">Error icon</span>
+			{#if t.variant === 'danger'}
+				<CloseCircleSolid class="h-5 w-5" />
+				<span class="sr-only">Error icon</span>
+			{:else if t.variant === 'succes'}
+				<CheckCircleSolid class="h-5 w-5" />
+				<span class="sr-only">Succes icon</span>
+			{/if}
 		</svelte:fragment>
 		{#if t.title}
 			<h1 class="font-medium text-white">{t.title}</h1>
--- app/src/lib/toastStore.ts
+++ app/src/lib/toastStore.ts
@@ -3,13 +3,7 @@ import { writable } from "svelte/store";
 export type toast = {
     title: string | null
     text: string
-    variant: "danger" | null 
-}
-
-const defaultOpts: toast = {
-    title: null,
-    text: "",
-    variant: null,
+    variant: "danger" | "succes" | null 
 }
 
 export const toastStore = writable<toast[]>([])
