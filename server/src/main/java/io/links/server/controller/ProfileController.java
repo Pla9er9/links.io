133ed393d0a69c56dc7e93da595ab160483e4ae0
Pla9er <drugiekonto_99@o2.pl>
2024-06-09 23:28:09 +0200

    change: multiple imports to one

--- server/src/main/java/io/links/server/controller/ProfileController.java
+++ server/src/main/java/io/links/server/controller/ProfileController.java
@@ -5,10 +5,7 @@ import io.links.server.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/profile/")
