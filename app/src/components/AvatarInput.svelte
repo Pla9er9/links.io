3f7abed481591ce6c9bb7f94f79de65ede53ddca
Pla9er <drugiekonto_99@o2.pl>
2024-06-09 23:34:26 +0200

    create: AvatarInput component for picking images to upload

---
+++ app/src/components/AvatarInput.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Input } from "flowbite-svelte";
+
+    export let file
+    export let onChange: (e: Event) => void = () => {}
+</script>
+
+<label for="avatar-input" class="pointer">
+    Upload avatar
+</label>
+<Input id="avatar-input" on:change={onChange} class="hidden" type="file"/>
+
+<style lang="scss">
+    label {
+        font-size: 12px;
+        font-weight: 500;
+        color: #cfcece;
+        border: solid 1px #cfcece;
+        padding: 8px 12px;
+        border-radius: 7px;
+        cursor: pointer;
+    }
+</style>
